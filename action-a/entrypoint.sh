a426e186e58d9cf6c5a373ca4f91072be82a9b53
beautidata <beautifuldatadev@gmail.com>
2021-04-15 19:25:42 -0500

    remove parens from message

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo hi my name is (what) my name is (who) my name is (chicka-chicka) $INPUT_MY_NAME"
+sh -c "echo hi my name is -what- my name is -who- my name is -chicka-chicka- $INPUT_MY_NAME"
